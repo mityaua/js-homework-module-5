2c5d7c47ed8e67be063bb38758a4494b76fe13a3
Dmytro <mityaua@gmail.com>
2020-11-22 21:35:27 +0200

    Добавляет решение 2й задачи и условие 3й

--- js/5-2.js
+++ js/5-2.js
@@ -1,4 +1,19 @@
+// 1. Создаем класс User используя аналог функции-конструктора, куда принимаем три параметра с именем, возрастом и количеством подписчиков;
+// 2. Записываем в прототив User функцию getInfo, которая принимает параметры имени, возраста, к-ва подписчиков и возвращает шаблонную строку по условию, используя this;
+// 3. Также данная задача решается через обычную функцию контруктор, но от нас требуют решение именно через class;
+
 // Write code under this line
+class User {
+  constructor(name, age, followers) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+}
+
+User.prototype.getInfo = function (name, age, followers) {
+  return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`;
+};
 
 console.log(typeof User);
 // 'function'
@@ -10,6 +25,6 @@ console.log(mango.getInfo());
 console.log(typeof mango.getInfo);
 // 'function'
 
-const poly = new User( 'Poly', 3, 17);
+const poly = new User('Poly', 3, 17);
 console.log(poly.getInfo());
 // 'User Poly is 3 years old and has 17 followers'
---
+++ js/5-3.js
@@ -0,0 +1,35 @@
+// Write code under this line
+
+console.log(typeof Storage);
+// 'function'
+
+const goods = ['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор'];
+
+const storage = new Storage(goods);
+
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+// storage.removeItem('Пролонгер');
+// console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
