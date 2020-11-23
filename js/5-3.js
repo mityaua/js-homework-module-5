b8643517f36ad4a648e53793695420a6f7c9b83e
Dmytro <mityaua@gmail.com>
2020-11-23 10:33:13 +0200

    Добавляет решение 3й задачи

--- js/5-3.js
+++ js/5-3.js
@@ -1,21 +1,28 @@
+// 1. Добавляем класс Storage который принимает один параметр - массив и создаёт на основе его свойство, в контексте this;
+// 2. Добавляем в класс Storage методы getItems, addItem, removeItem;
+// 3. Метод getItems будет принимать текущий массив и возращать его;
+// 4. Метод addItem будет принимать элемент массива и возвращать массив с этим элементом;
+// 5. Метод removeItem будет принимать элемент массива, определять его индекс и возвращать массив без этого элемента;
+
 // Write code under this line
 class Storage {
   constructor(items) {
     this.items = items;
   }
+
+  getItems = function (items) {
+    return this.items;
+  };
+
+  addItem = function (item) {    
+    return this.items.push(item);
+  };
+
+  removeItem = function (item) {
+    const index = this.items.indexOf(item);
+    return this.items.splice(index, 1);
+  };
 }
-
-Storage.prototype.getItems = function (items) {
-  return this.items;
-};
-
-Storage.prototype.addItem = function (items) {
-  return this.items;
-};
-
-Storage.prototype.removeItem = function (items) {
-  return this.items;
-};
 
 console.log(typeof Storage);
 // 'function'
