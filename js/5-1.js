96a5fd88530687c137dbf335e06f939909b8f8ae
Dmytro <mityaua@gmail.com>
2020-12-04 11:12:39 +0200

    Убирает лишние параметры для Account.prototype.getInfo

--- js/5-1.js
+++ js/5-1.js
@@ -6,7 +6,7 @@ const Account = function (login, email) {
   (this.login = login), (this.email = email);
 };
 
-Account.prototype.getInfo = function (login, email) {
+Account.prototype.getInfo = function () {
   return `login : ${this.login}, email: ${this.email}`;
 };
 
